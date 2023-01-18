b89a0d9a7ad2bfcbf2f94b6428c0ff2b440868a3
galiano <edigaliano@gmail.com>
2023-01-18 15:36:03 -0300

    Configurando o Webpack

--- 01-github-explorer/src/index.jsx
+++ 01-github-explorer/src/index.jsx
@@ -1,11 +1,2 @@
 import React from "react";
-
-function app() {
-    return <h1>Hello World</h1>
-}
-
-const user = {
-    name: 'Edi',
-}
-
-console.log(user.address?.street)
+import { App } from "./App"
