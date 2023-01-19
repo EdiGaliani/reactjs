fbd1db9fa426402c52d3a622319b79f8025284b4
galiano <edigaliano@gmail.com>
2023-01-19 14:03:55 -0300

    Componentes

--- 01-github-explorer/src/App.jsx
+++ 01-github-explorer/src/App.jsx
@@ -1,5 +1,6 @@
+import { RepositoryList } from './components/RepositoryList';
 import './styles/global.scss';
 
 export function App() {
-    return <h1>Hello World, React is cool!</h1>
+    return <RepositoryList />
 }
