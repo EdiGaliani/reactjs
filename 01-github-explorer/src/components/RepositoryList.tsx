c4d0eea6db78e4eefdfb251272b77d6e6d24fbbd
galiano <edigaliano@gmail.com>
2023-01-21 13:46:27 -0300

    Componentes com Typescript

--- 01-github-explorer/src/components/RepositoryList.tsx
+++ 01-github-explorer/src/components/RepositoryList.tsx
@@ -2,10 +2,14 @@ import { useState, useEffect } from "react";
 import { RepositoryItem } from "./RepositoryItem";
 import '../styles/repositories.scss';
 
-//https://api.github.com/users/EdiGaliani/repos
+interface Repository {
+    name: string;
+    description: string;
+    html_url: string;
+}
 
 export function RepositoryList() {
-    const [repositories, setRepositories] = useState([])
+    const [repositories, setRepositories] = useState<Repository[]>([])
 
     useEffect(() => {
         fetch('https://api.github.com/users/EdiGaliani/repos')
@@ -18,7 +22,7 @@ export function RepositoryList() {
             <h1>Lista de Repositório</h1>
             <ul>
                 {repositories.map(repository => {
-                   return <RepositoryItem key= {repository.name} repository= {repository}/>
+                   return <RepositoryItem key= {repository.name} repository={repository}/>
                 })}
             
             </ul>
