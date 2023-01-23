08e9582d5ffc787685c27eeaf8ce9344e4054b95
galiano <edigaliano@gmail.com>
2023-01-23 16:59:06 -0300

    Termino da Componentização

--- dtmoney/src/components/Dashboard/index.tsx
+++ dtmoney/src/components/Dashboard/index.tsx
@@ -1,10 +1,12 @@
 import { Summary } from "../Summary";
+import { TransactionTable } from "../TransactionsTable";
 import { Container } from "./Styles";
 
 export function Dashboard() {
     return (
         <Container>
             <Summary />
+            <TransactionTable />
         </Container>
     )
 }
---
+++ dtmoney/src/components/TransactionsTable/index.tsx
@@ -0,0 +1,38 @@
+import { Container } from "./Styles";
+
+export function TransactionTable() {
+    return (
+        <Container>
+            <table>
+                <thead>
+                    <tr>
+                        <th>Título</th>
+                        <th>Valor</th>
+                        <th>Categoria</th>
+                        <th>Data</th>
+                    </tr>
+                </thead>
+                <tbody>
+                    <tr>
+                        <td>Desenvolvimento de Website</td>
+                        <td className="withdraw">R$12.000</td>
+                        <td>Desenvolvimento</td>
+                        <td>20/02/2023</td>
+                    </tr>
+                    <tr>
+                        <td>Aluguel</td>
+                        <td className="deposit">-R$2.000</td>
+                        <td>Casa</td>
+                        <td>03/02/2023</td>
+                    </tr>
+                    <tr>
+                        <td>Mercado</td>
+                        <td className="withdraw">R$12.000</td>
+                        <td>Vendas</td>
+                        <td>31/02/2023</td>
+                    </tr>
+                </tbody>
+            </table>
+        </Container>
+    )
+}
