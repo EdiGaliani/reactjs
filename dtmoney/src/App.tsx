73d5eb8b1e2328b62b71e9a453ab55777ec1a29d
galiano <edigaliano@gmail.com>
2023-01-26 10:42:51 -0300

    Estrutura do Modal Formulário

--- dtmoney/src/App.tsx
+++ dtmoney/src/App.tsx
@@ -3,6 +3,7 @@ import { Dashboard } from "./components/Dashboard";
 import { Header } from "./components/Header";
 import Modal from 'react-modal'
 import { GlobalStyle } from "./styles/global";
+import { NewTransactionModal } from "./components/NewTransactionModal";
 
 Modal.setAppElement('#root');
 
@@ -20,9 +21,10 @@ export function App() {
     <>
       <Header onOpenNewTransactionModal={handleOpenTransactionModal} />
       <Dashboard />
-      <Modal isOpen={isNewTransactionModalOpen} onRequestClose={handleCloseTransactionModal}>
-                    <h2>Cadastrar Transação</h2>
-      </Modal>
+      <NewTransactionModal 
+        isOpen={isNewTransactionModalOpen}
+        onRequestClose={handleCloseTransactionModal}
+      />
       <GlobalStyle />
     </>
   );
---
+++ dtmoney/src/components/NewTransactionModal/index.tsx
@@ -0,0 +1,28 @@
+import { useState } from 'react';
+import Modal from 'react-modal';
+import { Container } from './styles';
+
+interface NewTransactionModalProps {
+    isOpen: boolean;
+    onRequestClose: () => void;
+}
+
+export function NewTransactionModal({ isOpen, onRequestClose }: NewTransactionModalProps) {
+
+    return ( 
+        <Modal 
+            isOpen={isOpen} 
+            onRequestClose={onRequestClose}
+            overlayClassName="react-modal-overlay"
+            className="react-modal-contant"
+        >
+            <Container>
+                <h2>Cadastrar Transação</h2>
+                <input placeholder="Título" />
+                <input placeholder="Valor" />
+                <input placeholder="Categoria" />
+                <button type="submit">Cadastrar</button>
+            </Container>
+        </Modal>
+    )
+}
