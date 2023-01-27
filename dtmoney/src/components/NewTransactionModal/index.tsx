9021340600e452c876a8388f1718cca540bf9c58
galiano <edigaliano@gmail.com>
2023-01-27 11:47:26 -0300

    Cores dos Botões

--- dtmoney/src/components/NewTransactionModal/index.tsx
+++ dtmoney/src/components/NewTransactionModal/index.tsx
@@ -32,6 +32,7 @@ export function NewTransactionModal({ isOpen, onRequestClose }: NewTransactionMo
                         type="button"
                         onClick={() => { setType('deposit'); }}
                         isActive={ type === 'deposit'}
+                        activeColor="green"
                     >
                        <img src={incomeImg} alt="Entrada" />
                        <span>Entrada</span> 
@@ -40,6 +41,7 @@ export function NewTransactionModal({ isOpen, onRequestClose }: NewTransactionMo
                         type="button"
                         onClick={() => { setType('withdraw'); }}
                         isActive={ type === 'withdraw'}
+                        activeColor="red"
                     >
                        <img src={outcomeImg} alt="Saída" />
                        <span>Saída</span> 
